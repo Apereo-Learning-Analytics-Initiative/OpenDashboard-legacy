1ce7853ff3f36626d8ec687f50efecb177b3bcba
Aaron Zeckoski <azeckoski@gmail.com>
2014-07-21 17:47:35 -0400

    restructuring

--- src/main/java/ltistarter/oauth/OAuthUtils.java
+++ src/main/java/ltistarter/oauth/OAuthUtils.java
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package ltistarter.utils;
+package ltistarter.oauth;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
