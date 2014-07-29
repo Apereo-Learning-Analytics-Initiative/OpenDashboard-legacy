de3760ff42d65ed8a67beb17076834932f5c70be
Aaron Zeckoski <azeckoski@gmail.com>
2014-07-28 21:51:28 -0400

    Got the unit tests working with Auth again

--- src/test/java/ltistarter/BaseApplicationTest.java
+++ src/test/java/ltistarter/BaseApplicationTest.java
@@ -15,14 +15,26 @@
 package ltistarter;
 
 import ltistarter.config.ApplicationConfig;
+import ltistarter.oauth.MyOAuthAuthenticationHandler;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.context.ConfigurableWebApplicationContext;
 
 import javax.annotation.PostConstruct;
+import java.security.Principal;
+import java.util.Collection;
+import java.util.HashSet;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
@@ -50,6 +62,34 @@ public abstract class BaseApplicationTest {
         assertNotNull(context);
         assertNotNull(applicationConfig);
         assertTrue(applicationConfig.getEnvironment().acceptsProfiles("testing"));
+    }
+
+    /**
+     * Makes a new session which contains authentication roles,
+     * this allows us to test requests with varying types of security
+     *
+     * @param username the username to set for the session
+     * @param roles    all the roles to grant for this session
+     * @return the session object to pass to mockMvc (e.g. mockMvc.perform(get("/").session(session))
+     */
+    public MockHttpSession makeAuthSession(String username, String... roles) {
+        if (StringUtils.isEmpty(username)) {
+            username = "azeckoski";
+        }
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, SecurityContextHolder.getContext());
+        Collection<GrantedAuthority> authorities = new HashSet<>();
+        if (roles != null && roles.length > 0) {
+            for (String role : roles) {
+                authorities.add(new SimpleGrantedAuthority(role));
+            }
+        }
+        //Authentication authToken = new UsernamePasswordAuthenticationToken("azeckoski", "password", authorities); // causes a NPE when it tries to access the Principal
+        Principal principal = new MyOAuthAuthenticationHandler.NamedOAuthPrincipal(username, authorities,
+                "key", "signature", "HMAC-SHA-1", "signaturebase", "token");
+        Authentication authToken = new UsernamePasswordAuthenticationToken(principal, null, authorities);
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+        return session;
     }
 
 }
--- src/test/java/ltistarter/controllers/AppControllersTest.java
+++ src/test/java/ltistarter/controllers/AppControllersTest.java
@@ -23,19 +23,13 @@ import org.junit.runner.RunWith;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import java.util.Collection;
-import java.util.HashSet;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
@@ -63,11 +57,12 @@ public class AppControllersTest extends BaseApplicationTest {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
                 .addFilters(springSecurityFilter)
                 .build();
+        context.getServletContext().setAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE, context);
     }
 
     @Test
     public void testLoadRoot() throws Exception {
-        // Test basic home controller request
+        // Test basic home controller request (no session, no user)
         MvcResult result = this.mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
@@ -75,18 +70,14 @@ public class AppControllersTest extends BaseApplicationTest {
         String content = result.getResponse().getContentAsString();
         assertNotNull(content);
         assertTrue(content.contains("Hello Spring Boot"));
-        assertTrue(content.contains("Form Login endpoint")); // TODO fails!
+        assertTrue(content.contains("Form Login endpoint"));
     }
 
     @Test
-    @Ignore
     public void testLoadRootWithAuth() throws Exception {
-        Collection<GrantedAuthority> authorities = new HashSet<>();
-        authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
-        Authentication authToken = new UsernamePasswordAuthenticationToken("azeckoski", "password", authorities);
-        SecurityContextHolder.getContext().setAuthentication(authToken);
-        // Test basic home controller request
-        MvcResult result = this.mockMvc.perform(get("/"))
+        // Test basic home controller request with a session and logged in user
+        MockHttpSession session = makeAuthSession("azeckoski", "ROLE_USER");
+        MvcResult result = this.mockMvc.perform(get("/").session(session))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andReturn();
@@ -97,17 +88,27 @@ public class AppControllersTest extends BaseApplicationTest {
     }
 
     @Test
+    public void testLoadFormWithAuth() throws Exception {
+        // Test form controller request with a session and logged in user
+        MockHttpSession session = makeAuthSession("azeckoski", "ROLE_USER");
+        MvcResult result = this.mockMvc.perform(get("/form").session(session))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andReturn();
+        String content = result.getResponse().getContentAsString();
+        assertNotNull(content);
+        assertTrue(content.contains("Hello Spring Boot"));
+        assertTrue(content.contains("only shown to users (ROLE_USER)"));
+        assertTrue(content.contains("Logout")); // logout button
+    }
+
+    @Test
     @Ignore
     public void testLoadLTI() throws Exception {
         // test minimal LTI launch
-        Collection<GrantedAuthority> authorities = new HashSet<>();
-        authorities.add(new SimpleGrantedAuthority("ROLE_OAUTH"));
-        authorities.add(new SimpleGrantedAuthority("ROLE_LTI"));
-        authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
-        Authentication authToken = new UsernamePasswordAuthenticationToken("azeckoski", "password", authorities);
-        SecurityContextHolder.getContext().setAuthentication(authToken);
+        MockHttpSession session = makeAuthSession("azeckoski", "ROLE_LTI", "ROLE_OAUTH", "ROLE_USER");
         MvcResult result = this.mockMvc.perform(
-                post("/lti1p")
+                post("/lti1p").session(session)
                         .param(LTIRequest.LTI_VERSION, LTIRequest.LTI_VERSION_1P0)
                         .param(LTIRequest.LTI_MESSAGE_TYPE, LTIRequest.LTI_MESSAGE_TYPE_BASIC)
                         .param(LTIRequest.LTI_CONSUMER_KEY, "key")
