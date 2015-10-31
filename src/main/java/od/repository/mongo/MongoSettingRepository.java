04b46d6c58bccbab67f9e0d43daa07b2633570e8
Gary Gilbert <gjgilbert@gmail.com>
2015-10-31 09:30:08 -0400

    bug fix for missing profile

--- src/main/java/od/repository/mongo/MongoSettingRepository.java
+++ src/main/java/od/repository/mongo/MongoSettingRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import od.framework.model.Setting;
 import od.repository.SettingRepositoryInterface;
 
-@Profile("mongo")
+@Profile({"mongo","mongo-multitenant"})
 public interface MongoSettingRepository extends SettingRepositoryInterface, MongoRepository<Setting, String> {
 }
