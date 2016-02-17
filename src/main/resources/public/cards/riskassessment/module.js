c59232305cc685ddfd9db8c8f3b55790a9ed3854
Gary Gilbert <gjgilbert@gmail.com>
2016-02-17 15:59:38 -0500

    first pass through module instance view dashboard

---
+++ src/main/resources/public/cards/riskassessment/module.js
@@ -0,0 +1,166 @@
+/*******************************************************************************
+ * Copyright 2015 Unicon (R) Licensed under the
+ * Educational Community License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *******************************************************************************/
+(function(angular, Math, moment) {
+'use strict';
+    
+angular
+.module('od.cards.riskassessment', ['OpenDashboardRegistry', 'OpenDashboardAPI'])
+ .config(function(registryProvider){
+    registryProvider.register('riskassessment',{
+        title: 'Risk Asessment',
+        description: '',
+        imgUrl: '',
+        cardType: 'riskassessment',
+        styleClasses: 'od-card col-xs-12',
+	    config: [],
+	    requires: ['ROSTER', 'MODELOUTPUT'],
+	    uses: []
+    });
+ })
+ .controller('RiskAssessmentController', function($scope, $translate, $translatePartialLoader, $log, _, SessionService, EventService, RosterService, ModelOutputDataService) {
+	 
+   $translatePartialLoader.addPart('risk-assessment');
+   $translate
+   .refresh()
+     .then(function() {
+    	 
+       $scope.courses = null;
+       $scope.filtered = null;
+       
+       $scope.activeCourse = SessionService.getCourse();       
+
+	   $scope.ALL_STUDENTS = {'id':0, 'label':'ALL STUDENTS', 'translationValue':$translate.instant('RA_LABEL_ALL_STUDENTS')};
+	   $scope.HIGH_RISK = {'id':1, 'label':'HIGH RISK', 'translationValue':$translate.instant('RA_LABEL_HIGH_RISK')};
+	   $scope.MEDIUM_RISK = {'id':2, 'label':'MEDIUM RISK', 'translationValue':$translate.instant('RA_LABEL_MEDIUM_RISK')};
+	   $scope.LOW_RISK = {'id':3, 'label':'LOW RISK', 'translationValue':$translate.instant('RA_LABEL_LOW_RISK')};
+	   $scope.NO_RISK = {'id':4, 'label':'NO RISK', 'translationValue':$translate.instant('RA_LABEL_NO_RISK')};
+   
+	   $scope.categories = [$scope.ALL_STUDENTS, $scope.HIGH_RISK, $scope.MEDIUM_RISK, $scope.LOW_RISK, $scope.NO_RISK];
+	   $scope.groupings = [$scope.ALL_STUDENTS.label, $scope.HIGH_RISK.label, $scope.MEDIUM_RISK.label, $scope.LOW_RISK.label, $scope.NO_RISK.label];
+	   $scope.grouping = $scope.groupings[0];
+   
+   
+
+ $scope.model_output = [
+   {"output":{"RMN_SCORE_PARTIAL":"82.6232","RC_FINAL_GRADE":"1.3","R_ASSMT_SUB":null,"COURSE_ID":"13","PERCENTILE":0,"SAT_MATH":650,"ENROLLMENT":5,"SAT_VERBAL":600,"R_CONTENT_READ":"1.0","ACADEMIC_RISK":1,"APTITUDE_SCORE":1190,"R_ASN_SUB":"1.0","ID":1,"MODEL_RISK_CONFIDENCE":"LOW RISK","R_ASN_READ":null,"AGE":22,"ONLINE_FLAG":false,"R_FORUM_POST":null,"R_LESSONS_VIEW":null,"FAIL_PROBABILITY":"0.596","PASS_PROBABILITY":"0.404","ALTERNATIVE_ID":"8","RC_GENDER":2,"GPA_CUMULATIVE":"2.2182","RC_CLASS_CODE":"4","STANDING":"0","GPA_SEMESTER":"2.6600","R_FORUM_READ":null,"RC_ENROLLMENT_STATUS":null,"SUBJECT":"Analysis","R_SESSIONS":"1.5663","R_ASSMT_TAKE":null,"RMN_SCORE":"82.6232"},"createdDate":1446481125659},
+   {"output":{"RMN_SCORE_PARTIAL":"125.0927","RC_FINAL_GRADE":"3.7","R_ASSMT_SUB":null,"COURSE_ID":"13","PERCENTILE":94,"SAT_MATH":660,"ENROLLMENT":5,"SAT_VERBAL":620,"R_CONTENT_READ":"2.2","ACADEMIC_RISK":2,"APTITUDE_SCORE":1280,"R_ASN_SUB":null,"ID":2,"MODEL_RISK_CONFIDENCE":"NO RISK","R_ASN_READ":null,"AGE":22,"ONLINE_FLAG":false,"R_FORUM_POST":null,"R_LESSONS_VIEW":null,"FAIL_PROBABILITY":"0.000","PASS_PROBABILITY":"1.000","ALTERNATIVE_ID":"4","RC_GENDER":2,"GPA_CUMULATIVE":"3.8729","RC_CLASS_CODE":"1","STANDING":"2","GPA_SEMESTER":"3.6800","R_FORUM_READ":null,"RC_ENROLLMENT_STATUS":null,"SUBJECT":"Analysis","R_SESSIONS":"1.3253","R_ASSMT_TAKE":null,"RMN_SCORE":"125.0927"},"createdDate":1446481125668},
+   {"output":{"RMN_SCORE_PARTIAL":"121.4125","RC_FINAL_GRADE":"3.3","R_ASSMT_SUB":null,"COURSE_ID":"13","PERCENTILE":92,"SAT_MATH":590,"ENROLLMENT":5,"SAT_VERBAL":580,"R_CONTENT_READ":"1.0","ACADEMIC_RISK":2,"APTITUDE_SCORE":1170,"R_ASN_SUB":null,"ID":3,"MODEL_RISK_CONFIDENCE":"NO RISK","R_ASN_READ":null,"AGE":21,"ONLINE_FLAG":false,"R_FORUM_POST":null,"R_LESSONS_VIEW":null,"FAIL_PROBABILITY":"0.000","PASS_PROBABILITY":"1.000","ALTERNATIVE_ID":"6","RC_GENDER":1,"GPA_CUMULATIVE":"3.5438","RC_CLASS_CODE":"4","STANDING":"2","GPA_SEMESTER":"3.6500","R_FORUM_READ":null,"RC_ENROLLMENT_STATUS":null,"SUBJECT":"Analysis","R_SESSIONS":"1.0643","R_ASSMT_TAKE":null,"RMN_SCORE":"121.4125"},"createdDate":1446481125672},
+   {"output":{"RMN_SCORE_PARTIAL":"93.6217","RC_FINAL_GRADE":"2.0","R_ASSMT_SUB":null,"COURSE_ID":"13","PERCENTILE":87,"SAT_MATH":600,"ENROLLMENT":5,"SAT_VERBAL":570,"R_CONTENT_READ":"0.6","ACADEMIC_RISK":2,"APTITUDE_SCORE":1170,"R_ASN_SUB":null,"ID":4,"MODEL_RISK_CONFIDENCE":"NO RISK","R_ASN_READ":null,"AGE":22,"ONLINE_FLAG":false,"R_FORUM_POST":null,"R_LESSONS_VIEW":null,"FAIL_PROBABILITY":"0.272","PASS_PROBABILITY":"0.728","ALTERNATIVE_ID":"5","RC_GENDER":2,"GPA_CUMULATIVE":"2.5000","RC_CLASS_CODE":"3","STANDING":"0","GPA_SEMESTER":"2.3400","R_FORUM_READ":null,"RC_ENROLLMENT_STATUS":null,"SUBJECT":"Analysis","R_SESSIONS":"0.8434","R_ASSMT_TAKE":null,"RMN_SCORE":"93.6217"},"createdDate":1446481125673},
+   {"output":{"RMN_SCORE_PARTIAL":"77.2500","RC_FINAL_GRADE":"1.0","R_ASSMT_SUB":null,"COURSE_ID":"13","PERCENTILE":0,"SAT_MATH":0,"ENROLLMENT":5,"SAT_VERBAL":0,"R_CONTENT_READ":"0.3","ACADEMIC_RISK":1,"APTITUDE_SCORE":0,"R_ASN_SUB":null,"ID":5,"MODEL_RISK_CONFIDENCE":"MEDIUM RISK","R_ASN_READ":null,"AGE":36,"ONLINE_FLAG":false,"R_FORUM_POST":null,"R_LESSONS_VIEW":null,"FAIL_PROBABILITY":"0.884","PASS_PROBABILITY":"0.116","ALTERNATIVE_ID":"7","RC_GENDER":1,"GPA_CUMULATIVE":"2.3350","RC_CLASS_CODE":"4","STANDING":"0","GPA_SEMESTER":"1.7667","R_FORUM_READ":null,"RC_ENROLLMENT_STATUS":null,"SUBJECT":"Analysis","R_SESSIONS":"0.2008","R_ASSMT_TAKE":null,"RMN_SCORE":"77.2500"},"createdDate":1446481125674}];
+ 
+ $scope.roster = [
+   {"id":"9c97383e-f730-448d-83af-86d0532215a7","user_id":"4","role":"Learner","person":{"contact_email_primary":"jp@test.com","name_given":"James","name_family":"Pedroia","name_full":"James Pedroia"}},
+   {"id":"1919256c-f269-4091-9888-bb8117dc4535","user_id":"9","role":"Instructor","person":{"contact_email_primary":"sean@teachers.com","name_given":"Sean","name_family":"McBride","name_full":"Sean McBride"}},
+   {"id":"130007ff-e8ae-4aac-a7b7-3e44cddac673","user_id":"7","role":"Learner","person":{"contact_email_primary":"shunting@test.com","name_given":"Skylar","name_family":"Hunting","name_full":"Skylar Hunting"}},
+   {"id":"780e8c61-851e-4881-89fa-0b718bee4293","user_id":"6","role":"Learner","person":{"contact_email_primary":"jwales@test.com","name_given":"Josie","name_family":"Wales","name_full":"Josie Wales"}},
+   {"id":"623429b1-ccc7-4682-9560-e8a09e526379","user_id":"5","role":"Learner","person":{"contact_email_primary":"lw@test.com","name_given":"Luke","name_family":"Walker","name_full":"Luke Walker"}},
+   {"id":"6458622f-d3ea-4deb-ad8e-6a663ea4c30b","user_id":"8","role":"Learner","person":{"contact_email_primary":"whunting@test.com","name_given":"Will","name_family":"Hunting","name_full":"Will Hunting"}}];
+ 
+ _.remove($scope.roster,function(member){return member.role == 'Instructor'});
+ 
+   _.forEach($scope.roster, function(obj){
+		var match = _.find($scope.model_output, function(o) {
+			return o.output['ALTERNATIVE_ID'].toString() == obj.user_id;
+		});
+		
+		if (match)
+		  _.merge(obj,match);
+   });
+   
+ 
+   
+ 
+ $scope.changeGrouping = function(grouping) {
+   $scope.grouping = grouping;
+   if (grouping == $scope.ALL_STUDENTS.label) {
+	   $scope.filterExpression = null;
+   }
+   else {
+	   $scope.filterExpression = grouping;
+   }
+ }
+ 
+ 
+ var riskCountFunction = function(modelOutput) {
+   return modelOutput.output.MODEL_RISK_CONFIDENCE;
+ }
+ 
+ var riskPercentageFunction = function(count,total) {
+   if (!count || !total) return 0;
+   return Math.floor((count/total)*100);
+ }
+ 
+ var modelOutputTotal = _.size($scope.model_output);
+ var model_output_counts = _.countBy($scope.model_output, riskCountFunction);
+ var highCount =  model_output_counts[$scope.HIGH_RISK.label] ? model_output_counts[$scope.HIGH_RISK.label] : 0;
+ var mediumCount = model_output_counts[$scope.MEDIUM_RISK.label] ? model_output_counts[$scope.MEDIUM_RISK.label] : 0;
+ var lowCount = model_output_counts[$scope.LOW_RISK.label] ? model_output_counts[$scope.LOW_RISK.label] : 0;
+ var noCount = model_output_counts[$scope.NO_RISK.label] ? model_output_counts[$scope.NO_RISK.label] : 0;
+ 
+ $scope.counts = {};
+ $scope.counts[$scope.ALL_STUDENTS.label] = $scope.roster.length;
+ $scope.counts[$scope.HIGH_RISK.label] = highCount;
+ $scope.counts[$scope.MEDIUM_RISK.label] = mediumCount;
+ $scope.counts[$scope.LOW_RISK.label] = lowCount;
+ $scope.counts[$scope.NO_RISK.label] = noCount;
+
+ $scope.highRiskPercentage = riskPercentageFunction(highCount, modelOutputTotal);
+ $scope.mediumRiskPercentage = riskPercentageFunction(mediumCount, modelOutputTotal);
+ $scope.lowRiskPercentage = riskPercentageFunction(lowCount, modelOutputTotal);
+ $scope.noRiskPercentage = riskPercentageFunction(noCount, modelOutputTotal);
+ 
+   // LEFT NAV
+
+   $scope.switchCourse = function(courseId) {
+     if ($scope.courses) {
+       var course = _.find($scope.courses,{'id':courseId});
+       if (course) {
+    	   $scope.activeCourse = course;
+       }
+     }
+   }
+
+   // TABLE
+
+   $scope.compareGroup = [];
+   $scope.toggleCompare = function(userId) {
+     var idx = $scope.compareGroup.indexOf(userId);
+     
+     if (idx > -1) {
+       $scope.compareGroup.splice(idx, 1);
+     }
+     else {
+       $scope.compareGroup.push(userId);
+     }
+     
+     $log.debug($scope.compareGroup);
+   }
+   
+   // DOUGHNUT
+   $scope.labels = [$scope.HIGH_RISK.translationValue, $scope.MEDIUM_RISK.translationValue, $scope.LOW_RISK.translationValue, $scope.NO_RISK.translationValue];
+   $scope.colors = ["#d9534f","#f0ad4e","#5bc0de","#5cb85c"]
+   $scope.data = [highCount,mediumCount,lowCount,noCount];
+ 
+   $scope.donutClick = function(points, evt) {
+	 var category = _.find($scope.categories, {'translationValue':points[0].label});
+	 $scope.changeGrouping(category.label);
+   }
+
+
+     }); // $translate.refresh.then
+ 
+});
+
+})(angular, Math, moment);
