1d76a82082c84f08d5c48854e0e19e90d64b9b85
Steven Cody <scody@unicon.net>
2019-04-24 15:19:45 -0600

    fixing

--- src/main/resources/public/assets/js/pulse/pulse.js
+++ src/main/resources/public/assets/js/pulse/pulse.js
@@ -65,6 +65,108 @@
     
     $scope.numberOfAtRiskStudents = 5;
     
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+new Chart(document.getElementById("bubble-chart"), {
+    type: 'bubble',
+    data: {
+      labels: "Africa",
+      datasets: [
+        {
+          label: ["China"],
+          backgroundColor: "rgba(255,221,50,0.2)",
+          borderColor: "rgba(255,221,50,1)",
+          data: [{
+            x: 21269017,
+            y: 5.245,
+            r: 15
+          }]
+        }, {
+          label: ["Denmark"],
+          backgroundColor: "rgba(60,186,159,0.2)",
+          borderColor: "rgba(60,186,159,1)",
+          data: [{
+            x: 258702,
+            y: 7.526,
+            r: 10
+          }]
+        }, {
+          label: ["Germany"],
+          backgroundColor: "rgba(0,0,0,0.2)",
+          borderColor: "#000",
+          data: [{
+            x: 3979083,
+            y: 6.994,
+            r: 15
+          }]
+        }, {
+          label: ["Japan"],
+          backgroundColor: "rgba(193,46,12,0.2)",
+          borderColor: "rgba(193,46,12,1)",
+          data: [{
+            x: 4931877,
+            y: 5.921,
+            r: 15
+          }]
+        }
+      ]
+    },
+
+    options: {
+
+    
+      title: {
+        display: true,
+        text: 'Predicted world population (millions) in 2050'
+      }, scales: {
+        yAxes: [{ 
+          scaleLabel: {
+            display: true,
+            labelString: "Happiness"
+          }
+        }],
+        xAxes: [{ 
+          scaleLabel: {
+            display: true,
+            labelString: "GDP (PPP)"
+          }
+        }]
+      }
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
     var riskColorClasses = [
       {
         'threshold':[0, .5],
@@ -945,6 +1047,22 @@ $scope.toggleActivityDetail = function (id) {
           });
 
         }
+        
+        
+        
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 
         function handleResize() {
           alignTables();
