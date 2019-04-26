61afa04ea0e1e1e81e2c28985ae7c45f790cea76
Eric Stenton <Eric.Stenton1@marist.edu>
2019-04-26 12:18:45 -0400

    Removed files

--- src/main/java/od/entrypoints/EntryPointProcessor.java
+++
@@ -1,8 +0,0 @@
-package od.entrypoints;
-
-import javax.servlet.http.HttpServletRequest;
-
-public interface EntryPointProcessor {
-  String post(HttpServletRequest request, String tenantId);
-  String get(HttpServletRequest request, String tenantId);
-}
--- src/main/java/od/exception/MethodNotImplementedException.java
+++
@@ -1,5 +0,0 @@
-package od.exception;
-
-public class MethodNotImplementedException extends Exception {
-
-}
