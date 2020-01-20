2cee083b9494fe00f32b745c141ad622730ced09
Steven Cody <scody@unicon.net>
2020-01-20 16:17:10 -0700

    ft cache

--- src/main/java/od/tasks/PulseCacheTask.java
+++ src/main/java/od/tasks/PulseCacheTask.java
@@ -8,6 +8,7 @@ import java.util.concurrent.CompletableFuture;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
